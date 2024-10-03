efb011ee6b0d23abe3e176fe2ad2a76eb4656074
vineet kumar <123523181+Vineet134@users.noreply.github.com>
2024-10-03 13:41:58 +0530

    Add files via upload

---
+++ Backend/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+require("dotenv").config();
+const { connection } = require("./Config/db");
+const { userRoute } = require("./Routes/user.route");
+const cors = require("cors");
+const app = express();
+
+// const PORT=process.env.PORT ||3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/chat", userRoute);
+
+app.listen(process.env.PORT, async () => {
+  await connection;
+  console.log(`server started on port ${process.env.PORT}`);
+});
+
+
